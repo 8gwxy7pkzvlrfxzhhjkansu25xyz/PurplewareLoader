3c71440977affee17ec294d0b9bac7c410240354
hhushed <phantoms629@gmail.com>
2025-09-25 15:53:36 +1200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,4 @@
--- Put this in a separate script, don't obfuscate it
 local Library = loadstring(game:HttpGetAsync("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))()
 local SaveManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/SaveManager.luau"))()
 local InterfaceManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/InterfaceManager.luau"))()
-
--- Then load your obfuscated main script
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://github.com/iloveskiddingcodeaha/PurplewareLoader/raw/refs/heads/main/Script.lua"))()
